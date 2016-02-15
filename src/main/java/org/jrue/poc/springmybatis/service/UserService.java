798c7344f300396050748abe71f399299bd2941c
Joel F. Ruelos; <ruelosjoel@gmail.com;>
2016-02-15 12:47:12 +0800

    Add UserDetailsService implementation of Spring for security

--- src/main/java/org/jrue/poc/springmybatis/service/UserService.java
+++ src/main/java/org/jrue/poc/springmybatis/service/UserService.java
@@ -1,13 +1,14 @@
 package org.jrue.poc.springmybatis.service;
 
 import org.jrue.poc.springmybatis.domain.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 /**
  * This service provides CRUD operations for {@link User} entity
  * @author jruelos
  *
  */
-public interface UserService extends BaseService<User, Long> {
+public interface UserService extends BaseService<User, Long>, UserDetailsService {
 
 	User findByName(String name);
 }
--- src/main/java/org/jrue/poc/springmybatis/service/UserServiceImpl.java
+++ src/main/java/org/jrue/poc/springmybatis/service/UserServiceImpl.java
@@ -1,31 +1,34 @@
 package org.jrue.poc.springmybatis.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.jrue.poc.springmybatis.domain.Role;
 import org.jrue.poc.springmybatis.domain.User;
 import org.jrue.poc.springmybatis.persistence.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Provide implementation of UserService interface which delegates
- * the implemented methods to User Repository.
+ * Provide implementation of UserService interface which delegates the
+ * implemented methods to User Repository.
  * 
  * @author jruelos
- *
+ * 
  */
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
 
-	private UserMapper userMapper;
-	
 	@Autowired
-	public UserServiceImpl(UserMapper userRepository) {
-		this.userMapper = userRepository;
-	}
-	
+	private UserMapper userMapper;
+
 	@Override
 	public boolean save(User persisted) {
 		return userMapper.save(persisted);
@@ -40,7 +43,6 @@ public class UserServiceImpl implements UserService {
 	public void delete(User deleted) {
 		userMapper.delete(deleted);
 	}
-
 
 	@Override
 	public List<User> findAll() {
@@ -60,5 +62,27 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User findByName(String name) {
 		return userMapper.findByName(name);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		User user = findByName(username);
+		org.springframework.security.core.userdetails.User userDetails = null;
+		if (user != null && user.getName() != null) {
+			userDetails = new org.springframework.security.core.userdetails.User(
+					username, user.getPassword(), authorities(user.getRoles()));
+		}
+		return userDetails;
+	}
+	
+	private List<GrantedAuthority> authorities(List<Role> roles) {
+		List<GrantedAuthority> auths = new ArrayList<GrantedAuthority>(roles.size());
+		
+		for (Role role : roles) {
+			auths.add(new SimpleGrantedAuthority(role.getRoleName()));
+		}
+		
+		return Collections.unmodifiableList(auths);
 	}
 }
