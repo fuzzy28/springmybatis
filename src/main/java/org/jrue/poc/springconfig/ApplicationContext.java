f4309f77c88cfa8ba0565fc2e13a540d06474efb
Joel F. Ruelos Jr. <ruelosjoel@gmail.com>
2016-03-06 00:32:53 +0800

    Move property overrider location to Application context

--- src/main/java/org/jrue/poc/springconfig/ApplicationContext.java
+++ src/main/java/org/jrue/poc/springconfig/ApplicationContext.java
@@ -1,17 +1,47 @@
 package org.jrue.poc.springconfig;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentPBEConfig;
+import org.jasypt.encryption.pbe.config.PBEConfig;
+import org.jasypt.spring31.properties.EncryptablePropertyOverrideConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Main Application Configuration
+ * 
  * @author jruelos
  *
  */
-@Import({PersistenceContext.class, LoggingAspect.class})
-@ComponentScan(basePackages = {"org.jrue.poc"})
+@Import({ PersistenceContext.class, LoggingAspect.class })
+@ComponentScan(basePackages = { "org.jrue.poc" })
 @EnableAspectJAutoProxy
 public class ApplicationContext {
 
+	@Bean
+	public static PBEConfig pbeConfig() {
+		EnvironmentPBEConfig envPbeConfig = new EnvironmentPBEConfig();
+		envPbeConfig.setAlgorithm("PBEWithMD5AndDES");
+		envPbeConfig.setPasswordSysPropertyName("ENCRYPTION_PASSWORD");
+		return envPbeConfig;
+	}
+
+	@Bean
+	public static StringEncryptor stringEncryptor() {
+		StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+		standardPBEStringEncryptor.setConfig(pbeConfig());
+		return standardPBEStringEncryptor;
+	}
+
+	@Bean
+	public static EncryptablePropertyOverrideConfigurer propertyOverider() {
+		EncryptablePropertyOverrideConfigurer propOverider = new EncryptablePropertyOverrideConfigurer(
+				stringEncryptor());
+		propOverider.setLocation(new ClassPathResource("database.properties"));
+		return propOverider;
+	}
 }
--- src/main/java/org/jrue/poc/springconfig/PersistenceContext.java
+++ src/main/java/org/jrue/poc/springconfig/PersistenceContext.java
@@ -2,18 +2,12 @@ package org.jrue.poc.springconfig;
 
 import javax.sql.DataSource;
 
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.EnvironmentPBEConfig;
-import org.jasypt.encryption.pbe.config.PBEConfig;
-import org.jasypt.spring31.properties.EncryptablePropertyOverrideConfigurer;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -50,7 +44,6 @@ public class PersistenceContext {
 	@Bean
 	@Primary
 	public PlatformTransactionManager transactionManager() {
-		;
 		return new DataSourceTransactionManager(getDatasource());
 	}
 
@@ -60,28 +53,5 @@ public class PersistenceContext {
 		factoryBean.setDataSource(getDatasource());
 		factoryBean.setTypeAliasesPackage("org.jrue.poc.springmybatis.domain");
 		return factoryBean;
-	}
-	
-	@Bean
-	public static PBEConfig pbeConfig() {
-		EnvironmentPBEConfig envPbeConfig = new EnvironmentPBEConfig();
-		envPbeConfig.setAlgorithm("PBEWithMD5AndDES");
-		envPbeConfig.setPasswordSysPropertyName("ENCRYPTION_PASSWORD");
-		return envPbeConfig;
-	}
-
-	@Bean
-	public static StringEncryptor stringEncryptor() {
-		StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
-		standardPBEStringEncryptor.setConfig(pbeConfig());
-		return standardPBEStringEncryptor;
-	}
-
-	@Bean
-	public static EncryptablePropertyOverrideConfigurer propertyOverider() {
-		EncryptablePropertyOverrideConfigurer propOverider = new EncryptablePropertyOverrideConfigurer(
-				stringEncryptor());
-		propOverider.setLocation(new ClassPathResource("database.properties"));
-		return propOverider;
 	}
 }
